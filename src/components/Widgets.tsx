6fc9885af16003b25b3b4d4a744015560711690f
Bishesh <bishesh.sunam@gmail.com>
2022-08-02 14:47:15 +1200

    hot fix, removed localStorage

--- src/components/Widgets.tsx
+++ src/components/Widgets.tsx
@@ -11,15 +11,10 @@ export default function Widgets({}: Props) {
   const dispatch = useAppDispatch();
   const { widget, error } = useAppSelector((state) => state.widgets);
   const [trending, setTrending] = useState<Widget[] | undefined>([]);
-  
-  
+
   useEffect(() => {
-    if (widget === null) {
+    if (widget.length === 0) {
       dispatch(getTrending());
-      setTrending(widget);
-    } else {
-      const data = JSON.parse(localStorage.getItem("trending") || "");
-      setTrending(data);
     }
   }, []);
 
@@ -27,32 +22,31 @@ export default function Widgets({}: Props) {
     <div className="hidden lg:inline col-span-2">
       <h1 className="font-bold p-2">#Top news nz</h1>
       <div className="overflow-auto scrollbar-hide max-h-screen">
-        {trending &&
-          trending.map((item, i) => (
-            <div
-              className="flex flex-col space-x-4 bg-white p-4 mr-4 space-y-4"
-              key={i}
-            >
-              <h2 className="text-md font-semibold">{item.title}</h2>
-              <img
-                loading="lazy"
-                src={item.image_url}
-                alt="cover"
-                className="max-h-30 rounded-lg shadow-sm object-contain self-start"
-              />
-              <p className="text-sm">
-                {item.snippet}{" "}
-                <a
-                  href={item.url}
-                  target="_blank"
-                  className="text-blue-500"
-                  rel="noopener"
-                >
-                  Read more
-                </a>
-              </p>
-            </div>
-          ))}
+        {widget.map((item, i) => (
+          <div
+            className="flex flex-col space-x-4 bg-white p-4 mr-4 space-y-4"
+            key={i}
+          >
+            <h2 className="text-md font-semibold">{item.title}</h2>
+            <img
+              loading="lazy"
+              src={item.image_url}
+              alt="cover"
+              className="max-h-30 rounded-lg shadow-sm object-contain self-start"
+            />
+            <p className="text-sm">
+              {item.snippet}{" "}
+              <a
+                href={item.url}
+                target="_blank"
+                className="text-blue-500"
+                rel="noopener"
+              >
+                Read more
+              </a>
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
